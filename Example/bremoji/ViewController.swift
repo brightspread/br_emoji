5524d6bfe845ca7aded9c5c2a607f2c3826591d1
brightspread <brightspread.jo@gmail.com>
2022-01-14 17:00:57 +0900

    Initial commit

---
+++ Example/bremoji/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  bremoji
+//
+//  Created by brightspread on 01/14/2022.
+//  Copyright (c) 2022 brightspread. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
